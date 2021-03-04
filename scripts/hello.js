1f8640ee5c71d58af7f0bf04c0a59e989281bf28
新島　駿 <niijima131229@docomo.ne.jp>
2021-03-04 15:58:01 +0900

    おみくじ

--- scripts/hello.js
+++ scripts/hello.js
@@ -4,7 +4,7 @@ module.exports = (robot) => {
 		const username = msg.message.user.name;
 		msg.send('Hello, ' + username);
 	});
-	obot.hear(/おみくじ/i, (msg) => {
+	robot.hear(/おみくじ/i, (msg) => {
 		const user_id = msg.message.user.id;
     const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
     const lot = lots[Math.floor(Math.random() * lots.length)];
